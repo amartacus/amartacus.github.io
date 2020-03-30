5bc7bbfa11850d2f0262d5d557c3978c8561bb83
Alex Martinez <almartinez@squarespace.com>
2020-03-30 13:45:59 -0400

    add new page

--- date-comparison/service-worker.js
+++ date-comparison/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f541500ebacc1f8e730cfa1826506ee4.js"
+  "/date-comparison/precache-manifest.2ecd05f015de9d0b49926ee48745a512.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/date-comparison/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
